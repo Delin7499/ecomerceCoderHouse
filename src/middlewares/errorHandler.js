c92daf39c33e78c310f78574feeec85daa1b8f69
Matias Miguel Barcelo Acuña <matias.barcelo@fing.edu.uy>
2023-11-05 20:24:59 -0300

    modifico error handler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,6 @@
 import { createResponse } from "../utils.js";
 export const errorHandler = (error, req, res, next) => {
-  console.log(`error ${error.message}`);
-  const status = error.status;
+  console.log(`Error :${error.message}`);
+  const status = error.status || 500;
   createResponse(res, status, error.message);
-  next();
 };
