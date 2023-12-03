851cf48a7e44364c657e84cd1b1d6925e13b7b14
Matias Miguel Barcelo Acuña <matias.barcelo@fing.edu.uy>
2023-12-03 16:00:46 -0300

    agrego metodo post en /mockingproducts
    este post agrega 100 productos ramdom a la base de datos

--- src/app.js
+++ src/app.js
@@ -15,94 +15,96 @@ import viewsRouter from "./routes/views.router.js";
 import userRouter from "./routes/user.router.js";
 import sessionRouter from "./routes/session.router.js";
 import ticketRouter from "./routes/ticket.router.js";
-import { getProducts } from "./controllers/mocking.controller.js";
+import { getProducts, postToDB } from "./controllers/mocking.controller.js";
 import { addLogger } from "./middlewares/logger.js";
 import { connectToMongoDB } from "./utils/mongoconnect.js";
 import { logger } from "./utils/logger.js";
 import { cpus } from "os";
-import cluster from "cluster";
+//import cluster from "cluster";
 import {
   CartService,
   CategoryService,
   MessageService,
   ProductService,
 } from "./repositories/index.js";
+import { brotliMiddleware } from "./middlewares/brotli.js";
 
 const numberOfProcessors = cpus().length;
 
-if (cluster.isPrimary) {
+/* if (cluster.isPrimary) {
   logger.info(`The number of cpus is ${numberOfProcessors}`);
   console.log(config);
   for (let i = 0; i < numberOfProcessors; i++) {
     cluster.fork();
   }
-} else {
-  const app = express();
-  app.use(addLogger);
+} else { */
+const app = express();
+app.use(addLogger);
+//app.use(brotliMiddleware);
 
-  connectToMongoDB();
+connectToMongoDB();
 
-  app.use(
-    session({
-      store: MongoStore.create({
-        mongoUrl: config.mongoURL,
-        ttl: 100000,
-      }),
-      secret: "coderSectret",
-      resave: false,
-      saveUninitialized: false,
-    })
-  );
+app.use(
+  session({
+    store: MongoStore.create({
+      mongoUrl: config.mongoURL,
+      ttl: 100000,
+    }),
+    secret: "coderSectret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-  initializePassport();
-  app.use(passport.initialize());
-  app.use(passport.session());
+initializePassport();
+app.use(passport.initialize());
+app.use(passport.session());
 
-  app.use(cookieParser("CoderSecurity"));
+app.use(cookieParser("CoderSecurity"));
 
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-  const httpServer = app.listen(config.port, () =>
-    logger.info(`Worker ${process.pid} listening on port ${config.port}`)
-  );
-  const socketServer = new Server(httpServer);
+const httpServer = app.listen(config.port, () =>
+  logger.info(`Process ${process.pid} listening on port ${config.port}`)
+);
+const socketServer = new Server(httpServer);
 
-  app.use((req, _, next) => {
-    req.context = { socketServer };
-    next();
+app.use((req, _, next) => {
+  req.context = { socketServer };
+  next();
+});
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", `./src/views`);
+app.set("view engine", "handlebars");
+
+app.use(express.static(`./src/public`));
+app.use("/api/ticket", ticketRouter);
+app.use(`/api/products`, productRouter);
+app.use(`/api/carts`, cartsRouter);
+app.use("/api/users", userRouter);
+app.use("/api/session", sessionRouter);
+app.get("/mockingproducts", getProducts);
+app.post("/mokingproducts", postToDB);
+app.use(`/`, viewsRouter);
+
+socketServer.on(`connection`, async (socket) => {
+  logger.info(`Client connected to Worker ${process.pid}: ${socket.id}`);
+  socket.emit(`products`, await ProductService.getAll());
+  socket.emit("categories", await CategoryService.getAll());
+  socket.emit(`nuevo_mensaje`, await MessageService.getAll());
+
+  socket.on("cartId", async (cartId) => {
+    const cart = await CartService.getById(cartId);
+    socket.emit("cart", cart);
   });
 
-  app.engine("handlebars", handlebars.engine());
-  app.set("views", `./src/views`);
-  app.set("view engine", "handlebars");
-
-  app.use(express.static(`./src/public`));
-  app.use("/api/ticket", ticketRouter);
-  app.use(`/api/products`, productRouter);
-  app.use(`/api/carts`, cartsRouter);
-  app.use("/api/users", userRouter);
-  app.use("/api/session", sessionRouter);
-  app.get("/mockingproducts", getProducts);
-  app.use(`/`, viewsRouter);
-
-  socketServer.on(`connection`, async (socket) => {
-    logger.info(`Client connected to Worker ${process.pid}: ${socket.id}`);
-    socket.emit(`products`, await ProductService.getAll());
-    socket.emit("categories", await CategoryService.getAll());
-    socket.emit(`nuevo_mensaje`, await MessageService.getAll());
-
-    socket.on("cartId", async (cartId) => {
-      const cart = await CartService.getById(cartId);
-      socket.emit("cart", cart);
-    });
-
-    socket.on(`mensaje`, async (data) => {
-      MessageService.create(data);
-      const messages = await MessageService.getAll();
-      socketServer.emit("nuevo_mensaje", messages);
-    });
+  socket.on(`mensaje`, async (data) => {
+    MessageService.create(data);
+    const messages = await MessageService.getAll();
+    socketServer.emit("nuevo_mensaje", messages);
   });
+});
 
-  app.use(errorHandler);
-}
+app.use(errorHandler);
